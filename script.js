e7a8c571ce258fa3072471f1c84ca20fe137294c
Jitesh117 <sahookumarjitesh@gmail.com>
2022-02-11 18:57:30 +0530

    completed the mobile design

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+
 function hide() {
-    let hide = document.getElementsByClassName("shareit");
-    hide.classList.toggle("hide")
+    let alt = document.getElementById("profile-alt");
+    let prof = document.getElementById("profile");
+    prof.style.display= 'none';
+    alt.style.display = 'flex';
+}
+function unhide()
+{let alt = document.getElementById("profile-alt");
+let prof = document.getElementById("profile");
+    prof.style.display= 'flex';
+    alt.style.display= 'none';
 }

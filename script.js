261687aebd3d32e42ef220f489ab1e816e5175d7
Jitesh117 <sahookumarjitesh@gmail.com>
2022-02-12 09:26:11 +0530

    modified the js file

--- script.js
+++ script.js
@@ -2,12 +2,15 @@
 function hide() {
     let alt = document.getElementById("profile-alt");
     let prof = document.getElementById("profile");
+    
     prof.style.display= 'none';
     alt.style.display = 'flex';
 }
 function unhide()
-{let alt = document.getElementById("profile-alt");
-let prof = document.getElementById("profile");
+{
+    let alt = document.getElementById("profile-alt");
+    let prof = document.getElementById("profile");
+    
     prof.style.display= 'flex';
     alt.style.display= 'none';
 }
